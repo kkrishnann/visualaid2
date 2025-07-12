7f8329223839a944e2664b3531d7fecdaa154793
kkrishnann <109610020+kkrishnann@users.noreply.github.com>
2025-07-12 16:58:06 -0400

    cleaned up extra text and added a header for interview questions
    final edit 7/12

--- reg-questions-script.js
+++ reg-questions-script.js
@@ -1,15 +1,11 @@
 // Set the question set to use here
 const selectedSet = 'Interview'; // change this if needed
 let questions = window.questionSets[selectedSet];
+const backBtn = document.getElementById('back-btn');
 
-//const interviewBtn = document.getElementById('interview-btn');
-//const grammarBtn = document.getElementById('grammar-btn');
 const nextBtn = document.getElementById('next-btn');
 const questionSection = document.getElementById('question-section');
 const questionText = document.getElementById('question-text');
-//const welcomeMessage = document.getElementById('welcome-message');
-//const grammarWelcMessage = document.getElementById('grammar-message');
-
 let currentQuestion = 0;
 
 function showQuestion(index) {
@@ -20,28 +16,15 @@ function showQuestion(index) {
   }, 100);
 }
 
-//interviewBtn.addEventListener('click', () => {
-//  welcomeMessage.style.display = 'none';
-//  interviewBtn.style.display = 'none';
-//  questionSection.style.display = 'block';
-//  grammarBtn.style.display = 'none'
-//  grammarWelcMessage.style.display = 'none'
-//  currentQuestion = 0;
-//  setTimeout(() => {
-//    showQuestion(currentQuestion);
-//  }, 500);
-//});
-
 nextBtn.addEventListener('click', () => {
   currentQuestion = (currentQuestion + 1) % questions.length;
-//  showQuestion(currentQuestion);
   setTimeout(() => {
     showQuestion(currentQuestion);
   }, 500);
 });
 
-//grammarBtn.addEventListener('click', () => {
-//  window.location.href = 'grammar.html';
-//});
-
 showQuestion(currentQuestion);
+
+backBtn.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
